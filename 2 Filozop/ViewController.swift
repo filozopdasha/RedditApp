7c2f9f7ed8a0a8e541ac5033c5601538d7e4ab99
filozopdasha <dariafilozop@gmail.com>
2025-03-11 03:30:29 +0200

    Initial Commit

---
+++ 2 Filozop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2 Filozop
+//
+//  Created by Dasha Filozop on 11.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
